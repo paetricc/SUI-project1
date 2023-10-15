468edcd348a1a4a086ab0d7fa1b22f0bdba7926c
David Chocholaty <david.chocholaty12@gmail.com>
2023-10-15 13:29:24 +0200

    sui-solution: add unoptimal done version
    This commit adds the working not optimal version of using std::map for
    saving states and the action from one to another.

--- sui-solution.cc
+++ sui-solution.cc
@@ -1,34 +1,56 @@
+#include <algorithm>
 #include <queue>
 #include <set>
 
 #include "search-strategies.h"
 
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
-    if (init_state.isFinal()) {
-        return {};
-    }
+    // TODO podle discordu (dedObed puvodni stav nemuze byt konecny)
+    // if (init_state.isFinal()) {
+    //    return {};
+    // }
 
     std::queue<SearchState> queue_open;
-    std::set<SearchState> set_closed = {init_state};
     std::vector<SearchAction> solution = {};
+
+
+    std::set<SearchState> set_closed = {init_state};
+    std::map<SearchState, std::pair<SearchState, SearchAction>> states_relations;
 
     // Push the first state into the queue_open.
     queue_open.push(init_state);
 
     while (!queue_open.empty()) {
         // Take the first node from the queue.
-        SearchState working_state(queue_open.front());
+        SearchState working_state = queue_open.front();
         queue_open.pop();
 
         // Expand the node.
         std::vector<SearchAction> actions = working_state.actions();
 
         for (SearchAction action : actions) {
-            // TODO: Fix invalid action move.
-            SearchState action_state(action.execute(working_state));
+            // TODO asi se da action state deklarovat jenom jednou a pak jen prirazovat
+            SearchState action_state = action.execute(working_state);
+
 
             // If the current state is a goal.
             if (action_state.isFinal()) {
+                std::cout << "Found final \n";
+
+                solution.push_back(action);
+
+                // TODO optimalizovat
+                auto parent = states_relations.find(working_state);
+
+                while (parent != states_relations.end()) {
+                    std::cout << "searching root\n";
+
+                    solution.push_back(parent->second.second);
+                    parent = states_relations.find(parent->second.first);
+                }
+
+                std::reverse(solution.begin(), solution.end());
+
                 return solution;
             }
 
@@ -37,9 +59,8 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
                 // Add the node to the queue and to the set.
                 set_closed.insert(action_state);
                 queue_open.push(action_state);
-
-                // Add the node to the solution.
-                solution.push_back(action);
+                // TODO asi nejak s pointerama a neukladat stavy dvakrat (spojit asi nejak s mnozinou closed)
+                states_relations.insert(std::make_pair(action_state, std::make_pair(working_state, action)));
             }
         }
     }
