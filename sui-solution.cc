54abe0d1a197e250a52a234c8414f515e9e5e794
David Chocholaty <david.chocholaty12@gmail.com>
2023-10-19 15:35:54 +0200

    sui-solution: add memory cleaning
    This commit adds memory cleaning for created objects of search actions
    and tree nodes. Due to that the child vector is added as a parameter
    into the TreeNode class.

--- sui-solution.cc
+++ sui-solution.cc
@@ -6,7 +6,6 @@
 #include "search-strategies.h"
 
 // https://stackoverflow.com/questions/3686588/implementing-a-tree-in-c
-
 template<typename T>
 class TreeNode
 {
@@ -14,46 +13,39 @@ public:
     TreeNode()
     = default;
 
-    /*
-    explicit TreeNode(T  value, TreeNode<T> parent)
-            : Value(std::move(value)), Parent(parent)
-    {
-    }*/
-
-    TreeNode(T  _value, TreeNode<T>* _parent)
-        : Value(std::move(_value)), Parent(_parent)
+    TreeNode(T*  _value, TreeNode<T>* _parent)
+        : Value(_value), Parent(_parent)
     {
     }
 
-    T Value;
+    ~TreeNode()
+    {
+        for (TreeNode<T>* child : Children) {
+            delete child;
+            child = nullptr;
+        }
+
+        delete Value;
+        Value = nullptr;
+    }
+
+    T* Value;
     TreeNode<T>* Parent;
-    //std::list<TreeNode<T> > Children;
+    std::vector<TreeNode<T>* > Children;
 };
 
 // TODO kontrola pameti
-// TODO pro objekty vytvorene pomoci new take odstranit je pomoci delete
+// TODO jestli pouzit nejaky shared ptr
+// TODO jestli se daji klasicky C-style ukazatele nahradit nejakou konstrukci z cpp
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
     std::queue<std::pair<SearchState, TreeNode<SearchAction>*>> queue_open;
     std::vector<SearchAction> solution = {};
+    std::set<SearchState> set_closed = {init_state};
 
-    std::set<SearchState> set_closed = {init_state}; // TODO jestli mozna nespojit set_closed a predecessors std::set<std::tuple<SearchState, SearchAction, SearchState>> - porovnat performance
-
-/*
-    for (int i = 0; i < 2; i++) {
-        TreeNode<int>* tree_node1 = new TreeNode<int>(1, nullptr);
-        std::cout << "tree node 1: " << tree_node1 << "\n";
-        //TreeNode<int> tree_node1(1, nullptr);
-        //std::cout << "tree node 1: " << &tree_node1 << "\n";
-
-    }*/
-
-    // TreeNode<SearchAction> parent_tree_node(nullptr, nullptr);
-
-    // TODO projit
-    // std::map<SearchState, std::pair<SearchState, SearchAction>> predecessors;
+    auto root_node = new TreeNode<SearchAction>(nullptr, nullptr);
 
     // Push the first state into the queue_open.
-    queue_open.emplace(init_state, nullptr);
+    queue_open.emplace(init_state, root_node);
 
     while (!queue_open.empty()) {
         // Take the first node from the queue.
@@ -67,47 +59,21 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
         for (SearchAction action : actions) {
             SearchState action_state = action.execute(working_state);
 
-            //std::cout << "Searching actions \n";
-
             // If the current state is a goal.
             if (action_state.isFinal()) {
-
-                //std::cout << "here\n";
-
                 solution.push_back(action);
-
                 TreeNode<SearchAction>* action_in_tree = working_action;
 
-                //std::cout << "action in tree: " << action_in_tree << "\n";
-                //std::cout << "bool value: " << (action_in_tree == nullptr) << "\n";
-                //std::cout << "action in tree parent: " << action_in_tree->Parent << "\n";
-
-
-                while (action_in_tree != nullptr) {
-                    //std::cout << "loop: " << action_in_tree << "\n";
-                    solution.push_back(action_in_tree->Value);
+                while (action_in_tree->Parent != nullptr) {
+                    //solution.push_back(action_in_tree->Value);
+                    solution.push_back(*(action_in_tree->Value));
                     action_in_tree = action_in_tree->Parent;
                 }
-                /*
-                auto parent = predecessors.find(working_state);
 
-                while (parent != predecessors.end()) {
-                    //std::cout << "searching root\n";
-
-                    // Add the previous action which ends in the current known state from the path.
-                    solution.push_back(parent->second.second);
-                    // Find the previous state from which the action begins.
-                    parent = predecessors.find(parent->second.first);
-                }
-                */
                 // Reverse the pointers of the vector to get the solution in the right order.
-
-
-
-
                 std::reverse(solution.begin(), solution.end());
 
-                //std::cout << "returning ...\n";
+                delete root_node;
 
                 return solution;
             }
@@ -117,31 +83,16 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
                 // Add the node to the queue and to the set.
                 set_closed.insert(action_state);
 
-                //auto tree_node = new TreeNode<SearchAction>(action, working_action);
-                auto tree_node = new TreeNode<SearchAction>(action, working_action);
-                //std::cout << "Tree Node: " << tree_node << "\n";
-/*
-                std::cout << "Action: " << action << "\n";
-                std::cout << "Action in tree: " << tree_node->Value << "\n";
-                std::cout << "Parent: " << working_action << "\n";
-                std::cout << "Parent in tree: " << tree_node->Parent << "\n";
-                std::cout << "\n";
+                auto tree_action = new SearchAction(action);
+                auto tree_node = new TreeNode<SearchAction>(tree_action, working_action);
+                working_action->Children.push_back(tree_node);
 
-*/
-                //queue_open.emplace(action_state, &tree_node);
                 queue_open.emplace(action_state, tree_node);
-
-                //queue_open.emplace(action_state, nullptr);
-                // std::cout << &tree_node << "\n";
-
-                //predecessors.insert(std::make_pair(action, working_action));
-                //predecessors.insert(std::make_pair(action_state,std::make_pair(working_state, action)));
             }
         }
-
-        //std::cout << "----------------------------------------------------------------\n";
-        //std::cout << "----------------------------------------------------------------\n";
     }
+
+    delete root_node;
 
     // Return an empty list if the solution was not found.
     return {};
