664557f5d72b045fe0e5038c02e446887b42d9e1
Tomáš Bártů <tomasbartu00@gmail.com>
2023-10-21 00:51:09 +0200

    Code refactoring

--- sui-solution.cc
+++ sui-solution.cc
@@ -10,46 +10,51 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 }
 
 std::vector<SearchAction> DepthFirstSearch::solve(const SearchState &init_state) {
-    std::vector<SearchAction> result;
+    const u_char DEPTH = 0;
+    const u_int MEM_RESERVE = 50000000;
+
+    std::vector<SearchAction> vector_result;
     std::vector<SearchAction> vector_actions;
     std::stack<std::pair<SearchState, int>> state_stack;
-    int depth = 0;
-    state_stack.emplace(init_state, depth);
+
+    state_stack.emplace(init_state, DEPTH);
 
     while(!state_stack.empty()) {
-        if(getCurrentRSS() + 50000000 > this->mem_limit_)
+        // Memory check
+        if(getCurrentRSS() + MEM_RESERVE > this->mem_limit_)
             return {};
-
+        // Get new state to processing
         auto node = state_stack.top();
         state_stack.pop();
-
-        if(node.first.isFinal()) {
-            return result;
-        }
-
+        // Check if actual node result of game
+        if(node.first.isFinal())
+            return vector_result;
+        // Depth-limit occurred
         if(node.second == this->depth_limit_) {
             if(state_stack.empty())
                 continue;
-            size_t result_size = result.size();
-
+            // Depth changes so pop from vector_result and vector_action until
+            // vector_result size not equal to new state depth from top of state_stack
+            size_t result_size = vector_result.size();
             for(size_t i = state_stack.top().second; i <= result_size; i++){
-                if(!result.empty() && !vector_actions.empty()) {
-                    result.pop_back();
+                if(!vector_result.empty() && !vector_actions.empty()) {
+                    vector_result.pop_back();
                     vector_actions.pop_back();
                 }
             }
+            // New possible result action to vector_result
             if(!vector_actions.empty())
-                result.emplace_back(vector_actions.back());
+                vector_result.emplace_back(vector_actions.back());
+
             continue;
         }
-
-        auto actions = node.first.actions();
-        for(auto action : actions) {
-            state_stack.emplace(action.execute(node.first), node.second+1);
+        // Expand node and save actions and states to vector_actions and state_stack
+        for(auto action : node.first.actions()) {
+            state_stack.emplace(action.execute(node.first), node.second + 1);
             vector_actions.emplace_back(action);
         }
-
-        result.emplace_back(vector_actions.back());
+        // New possible result action to vector_result
+        vector_result.emplace_back(vector_actions.back());
     }
 	return {};
 }
