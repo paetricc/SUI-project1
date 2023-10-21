78daf2d2ffc510ceabac0da64723f9d6d78059ba
David Chocholaty <david.chocholaty12@gmail.com>
2023-10-21 18:14:48 +0200

    sui-solution: add shared pointers
    This commit adds shared pointers for the implementation of the bfs with
    a tree.

--- sui-solution.cc
+++ sui-solution.cc
@@ -8,7 +8,6 @@
 
 constexpr u_int MEM_RESERVE = 50000000;
 
-// https://stackoverflow.com/questions/3686588/implementing-a-tree-in-c
 template<typename T>
 class TreeNode
 {
@@ -16,36 +15,25 @@ public:
     TreeNode()
     = default;
 
-    TreeNode(T*  _value, TreeNode<T>* _parent)
-        : Value(_value), Parent(_parent)
+    TreeNode(std::shared_ptr<T>  _value, std::shared_ptr<TreeNode<T>> _parent)
+        : Value(std::move(_value)), Parent(std::move(_parent))
     {
     }
 
     ~TreeNode()
-    {
-        for (TreeNode<T>* child : Children) {
-            delete child;
-            child = nullptr;
-        }
+    = default;
 
-        delete Value;
-        Value = nullptr;
-    }
-
-    T* Value;
-    TreeNode<T>* Parent;
-    std::vector<TreeNode<T>* > Children;
+    std::shared_ptr<T> Value;
+    std::shared_ptr<TreeNode<T>> Parent;
 };
 
-// TODO kontrola pameti
-// TODO jestli pouzit nejaky shared ptr
-// TODO jestli se daji klasicky C-style ukazatele nahradit nejakou konstrukci z cpp
 std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_state) {
-    std::queue<std::pair<SearchState, TreeNode<SearchAction>*>> queue_open;
+    std::queue<std::pair<SearchState, std::shared_ptr<TreeNode<SearchAction>>>> queue_open;
     std::vector<SearchAction> solution = {};
     std::set<SearchState> set_closed = {init_state};
 
-    auto root_node = new TreeNode<SearchAction>(nullptr, nullptr);
+    std::shared_ptr<TreeNode<SearchAction>> root_node =
+            std::make_shared<TreeNode<SearchAction>>(nullptr, nullptr);
 
     // Push the first state into the queue_open.
     queue_open.emplace(init_state, root_node);
@@ -58,7 +46,7 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 
         // Take the first node from the queue.
         SearchState working_state = queue_open.front().first;
-        TreeNode<SearchAction>* working_action = queue_open.front().second;
+        std::shared_ptr<TreeNode<SearchAction>> working_action = queue_open.front().second;
         queue_open.pop();
 
         // Expand the node.
@@ -69,21 +57,16 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
 
             // If the current state is a goal.
             if (action_state.isFinal()) {
-                std::cout << "RSS: " << getCurrentRSS() << "\n";
-
                 solution.push_back(action);
-                TreeNode<SearchAction>* action_in_tree = working_action;
+                std::shared_ptr<TreeNode<SearchAction>> action_in_tree = working_action;
 
                 while (action_in_tree->Parent != nullptr) {
-                    //solution.push_back(action_in_tree->Value);
                     solution.push_back(*(action_in_tree->Value));
                     action_in_tree = action_in_tree->Parent;
                 }
 
                 // Reverse the pointers of the vector to get the solution in the right order.
                 std::reverse(solution.begin(), solution.end());
-
-                delete root_node;
 
                 return solution;
             }
@@ -93,16 +76,14 @@ std::vector<SearchAction> BreadthFirstSearch::solve(const SearchState &init_stat
                 // Add the node to the queue and to the set.
                 set_closed.insert(action_state);
 
-                auto tree_action = new SearchAction(action);
-                auto tree_node = new TreeNode<SearchAction>(tree_action, working_action);
-                working_action->Children.push_back(tree_node);
+                std::shared_ptr<SearchAction> tree_action = std::make_shared<SearchAction>(action);
+                std::shared_ptr<TreeNode<SearchAction>> tree_node =
+                        std::make_shared<TreeNode<SearchAction>>(tree_action, working_action);
 
                 queue_open.emplace(action_state, tree_node);
             }
         }
     }
-
-    delete root_node;
 
     // Return an empty list if the solution was not found.
     return {};
