7fa7f104ffd8cdb8f8ffff112b830490f49e05ab
David Chocholaty <david.chocholaty12@gmail.com>
2023-10-21 23:13:40 +0200

    sui-solution: add new A star version
    This commit adds a new version of the A star algorithm implementation.

--- sui-solution.cc
+++ sui-solution.cc
@@ -123,30 +123,22 @@ double StudentHeuristic::distanceLowerBound(const GameState &state) const {
     return cards_out_of_home;
 }
 
-typedef std::pair<SearchState, double> QUEUE_PAIR;
-
 std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
-    std::set<QUEUE_PAIR> queue_open;
+    std::set<std::pair<SearchState, double>> queue_open;
     std::vector<SearchAction> solution = {};
-    std::map<SearchState, int> map_costs_g = {std::make_pair(init_state, 0)}; // Used as a "closed" lookup table.
+    std::map<SearchState, double> map_costs_g = {std::make_pair(init_state, 0.0)}; // Used as a "closed" lookup table.
+    // TODO mozna muze byt unordered map, ale je potreba vytvorit hash
     std::map<SearchState, std::pair<SearchState, SearchAction>> predecessors;
 
-    // TODO asi jeste pouzit seznam closed: https://iopscience.iop.org/article/10.1088/1742-6596/1898/1/012047/pdf
-
-    double cost_f;
-
-    // TODO jestli osetrovat pointery
-    if (this->heuristic_) {
-        // f(n) = g(n) + h(n) = 0 + h(n) = h(n)
-        cost_f = compute_heuristic(init_state, *(this->heuristic_));
-        queue_open.insert(std::make_pair(init_state, cost_f));
-    } else {
-        // TODO snad by i nemelo nastat
-    }
+    queue_open.insert(std::make_pair(init_state, 0.0));
 
     while(!queue_open.empty()) {
+        // Memory check.
+        if (getCurrentRSS() + MEM_RESERVE > this->mem_limit_) {
+            return {};
+        }
+
         // Take the first node from the priority queue with the lowest value.
-        // https://stackoverflow.com/a/53619673
         std::pair<SearchState, double> queue_top = *std::min_element(queue_open.begin(), queue_open.end(), [](const auto& lhs, const auto& rhs) {
             return lhs.second < rhs.second;
         });
@@ -154,7 +146,6 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
         SearchState working_state = queue_top.first;
 
         if (working_state.isFinal()) {
-            // TODO Poznamka: skoro stejnej postup jako u BFS. treba by se dalo hodit do funkce
             auto parent = predecessors.find(working_state);
 
             while (parent != predecessors.end()) {
@@ -170,7 +161,7 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
             return solution;
         }
 
-        queue_open.erase(queue_open.begin());
+        queue_open.erase(queue_top);
 
         // Expand the node.
         std::vector<SearchAction> actions = working_state.actions();
@@ -178,18 +169,14 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
         for (SearchAction action : actions) {
             SearchState action_state = action.execute(working_state);
 
-            // Pro parenta ziskam hodnotu g(n) a vypoctu novou hodnotu pro potomky - g(n) + 1
+            // Pro parenta ziskam hodnotu g(n) a vypoctu novou hodnotu pro potomky: g(n) + 1
             auto state_cost_g = map_costs_g.find(working_state);
 
-            int cost_g = 0;
+            double tentative_cost_g = 0.0;
 
             if (state_cost_g != map_costs_g.end()) {
-                cost_g = state_cost_g->second;
-            } else {
-                // TODO - nemelo by pravdepodobne nastat
+                tentative_cost_g = state_cost_g->second + 1.0;
             }
-
-            double tentative_cost_g = cost_g + 1;
 
             // Tedka mam hodnotu g(n) pro potomky aktualniho stavu - tentative cost g.
 
@@ -199,51 +186,36 @@ std::vector<SearchAction> AStarSearch::solve(const SearchState &init_state) {
             // Pro childa
             state_cost_g = map_costs_g.find(action_state);
 
-            if (state_cost_g != map_costs_g.end()) {
-                // Child se nachazi jiz v mape. Ziskam jeho hodnotu g z mapy.
-                double child_cost_g = state_cost_g->second;
-
-                if (tentative_cost_g < child_cost_g) {
-                    // Nahrazuju hodnotu novou hodnotou.
-                    map_costs_g[action_state] = tentative_cost_g;
-
-                    // Nahradim i hodnotu v open.
-
-                    // 1. ziskam hodnotu heuristiky
-                    // f(n) = g(n) + h(n)
-                    cost_f = tentative_cost_g + compute_heuristic(action_state, *(this->heuristic_));
-                    // 2. vlozeni do fronty open.
-                    auto state = std::find_if(queue_open.begin(), queue_open.end(),
-                                              [&action_state](const QUEUE_PAIR & element){ return !(element.first < action_state) && !(action_state < element.first);} );
-
-                    if (state != queue_open.end()) {
-                        queue_open.erase(std::make_pair(state->first, state->second));
-                        queue_open.insert(std::make_pair(action_state, cost_f));
-                    }
-
-                    std::map<SearchState, std::pair<SearchState, SearchAction>>::iterator it = predecessors.find(action_state);
-                    if (it != predecessors.end()) {
-                        it->second = std::make_pair(working_state, action);
-                    } else {
-                        // TODO nemelo by nastat
-                    }
-                }
-            } else {
-                // Child neni v pame, vidime ho poprve - vkladam.
+            // Pokud child se nenachazi v mape.
+            if (state_cost_g == map_costs_g.end()) {
                 map_costs_g.emplace(action_state, tentative_cost_g);
 
-                // Neni ani v open, vlozim ho tam.
+                double cost_f = tentative_cost_g + compute_heuristic(action_state, *(this->heuristic_));
 
-                // 1. ziskam hodnotu heuristiky
-                // f(n) = g(n) + h(n)
-                cost_f = tentative_cost_g + compute_heuristic(action_state, *(this->heuristic_));
-                // 2. vlozeni do fronty open.
                 queue_open.insert(std::make_pair(action_state, cost_f));
                 predecessors.insert(std::make_pair(action_state,std::make_pair(working_state, action)));
+            } else if (tentative_cost_g < state_cost_g->second) {
+                // Child se nachazi v mape a nova hodnota je mensi nez stavajici.
+                state_cost_g->second = tentative_cost_g;
+
+                double cost_f = tentative_cost_g + compute_heuristic(action_state, *(this->heuristic_));
+
+                auto state = std::find_if(queue_open.begin(), queue_open.end(),
+                                          [&action_state](const std::pair<SearchState, double> & element){ return !(element.first < action_state) && !(action_state < element.first);} );
+
+                if (state != queue_open.end()) {
+                    queue_open.erase(std::make_pair(state->first, state->second));
+                    queue_open.insert(std::make_pair(action_state, cost_f));
+                }
+
+                auto it = predecessors.find(action_state);
+
+                if (it != predecessors.end()) {
+                    it->second = std::make_pair(working_state, action);
+                }
             }
         }
     }
-
 
 	return {};
 }
